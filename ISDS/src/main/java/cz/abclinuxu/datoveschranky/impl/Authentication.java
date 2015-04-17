018a5544d64a8f7992a0625574cebeed0e7c4d32
Milan Veselý <milan.vesely@jokiro.cz>
2015-04-17 13:36:31 +0200

    Odstraněna možnost přihlášení klientským certifikátem, odstraněna závislost na Oracle JDK

--- ISDS/src/main/java/cz/abclinuxu/datoveschranky/impl/Authentication.java
+++ ISDS/src/main/java/cz/abclinuxu/datoveschranky/impl/Authentication.java
@@ -1,14 +1,12 @@
 package cz.abclinuxu.datoveschranky.impl;
 
-import com.sun.xml.ws.developer.JAXWSProperties;
-import cz.abclinuxu.datoveschranky.common.impl.Config;
-import cz.abclinuxu.datoveschranky.common.impl.Utils;
-import java.security.KeyStore;
 import java.util.Map;
 import java.util.logging.Logger;
-import javax.net.ssl.SSLSocketFactory;
+
 import javax.xml.ws.BindingProvider;
 import javax.xml.ws.Service;
+
+import cz.abclinuxu.datoveschranky.common.impl.Config;
 
 /**
  *
@@ -18,13 +16,10 @@ import javax.xml.ws.Service;
 public abstract class Authentication {
 
     protected final Config config;
-    // protected SSLSocketFactory socketFactory = null;
     protected final Logger logger = Logger.getLogger(this.getClass().getName());
 
     protected Authentication(Config config) {
         this.config = config;
-        // KeyStore keyStore = config.getKeyStore();
-        // this.socketFactory = Utils.createSSLSocketFactory(keyStore);
     }
 
     public <T> T createService(Service serviceBuilder, Class<T> serviceClass, String servicePostfix) {
@@ -34,14 +29,8 @@ public abstract class Authentication {
     }
 
     protected void configureService(Map<String, Object> requestContext, String servicePostfix) {
-        requestContext.put(JAXWSProperties.SSL_SOCKET_FACTORY, this.createSSLSocketFactory());
         requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, config.getServiceURL() + servicePostfix);
         this.configureServiceOverride(requestContext, servicePostfix);
-    }
-
-    protected SSLSocketFactory createSSLSocketFactory() {
-        KeyStore keyStore = config.getKeyStore();
-        return Utils.createSSLSocketFactory(keyStore);
     }
 
     protected abstract void configureServiceOverride(Map<String, Object> requestContext, String servicePostfix);
--- ISDS/src/main/java/cz/abclinuxu/datoveschranky/impl/ClientCertAuthentication.java
+++ ISDS/src/main/java/cz/abclinuxu/datoveschranky/impl/ClientCertAuthentication.java
@@ -4,19 +4,12 @@
  */
 package cz.abclinuxu.datoveschranky.impl;
 
-import com.sun.xml.ws.developer.JAXWSProperties;
-import cz.abclinuxu.datoveschranky.common.impl.Config;
-import cz.abclinuxu.datoveschranky.common.impl.DataBoxException;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.SecureRandom;
 import java.util.Map;
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
+
 import javax.xml.ws.BindingProvider;
+
+import cz.abclinuxu.datoveschranky.common.impl.Config;
 
 /**
  *
@@ -29,7 +22,6 @@ public class ClientCertAuthentication extends Authentication {
 
     public ClientCertAuthentication(Config config, File certFile, String certPassword) {
         super(config);
-        KeyStore keyStore = config.getKeyStore();
         this.certFile = certFile;
         this.certPassword = certPassword;
     }
@@ -40,32 +32,9 @@ public class ClientCertAuthentication extends Authentication {
 
     @Override
     protected void configureService(Map<String, Object> requestContext, String servicePostfix) {
-        requestContext.put(JAXWSProperties.SSL_SOCKET_FACTORY, this.createSSLSocketFactory());
         requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, config.getServiceURLClientCert() + servicePostfix);
         this.configureServiceOverride(requestContext, servicePostfix);
     }
 
-    @Override
-    protected SSLSocketFactory createSSLSocketFactory() throws DataBoxException {
-        try {
-            // System.setProperty("https.protocols", "SSLv3");
-            // System.setProperty("javax.net.debug", "all");
-            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
-            KeyStore keyStore = KeyStore.getInstance("PKCS12");
-            // KeyStore keyStore = Utils.createTrustStore();
-            InputStream keyInput = new FileInputStream(certFile);
-            keyStore.load(keyInput, certPassword.toCharArray());
-            keyInput.close();
-            keyManagerFactory.init(keyStore, certPassword.toCharArray());
-            SSLContext context = SSLContext.getInstance("TLS");
-            context.init(keyManagerFactory.getKeyManagers(), null, new SecureRandom());
-            return context.getSocketFactory();
-        } catch (Exception ex) {
-            if (ex instanceof RuntimeException) {
-                throw (RuntimeException) ex;
-            } else {
-                throw new DataBoxException("Can't create SSLSocketFactory.", ex);
-            }
-        }
-    }
+
 }
